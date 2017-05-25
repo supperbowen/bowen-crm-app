2d48325ff01975ad98183eba8b5d150baa602c58
supperbowen <bowenliang@outlook.com>
2017-05-25 23:32:27 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
-cd ./usr/local/source/distss
+cd /usr/local/source/distss
 rm -rf dist
 unzip app.zip
 rm -rf /usr/local/source/crm/app
 cp -r dist/ /usr/local/source/crm/app
+
+rm -rf app.zip
+rm -rf dist
