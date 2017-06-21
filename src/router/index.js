19e07d972f0496265ea33ffdc4f57eca216acf9b
梁灿伦 <liangcl@nsw88.com>
2017-06-21 09:31:21 +0800

    修复构建失败

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,25 @@ import About from '@/modules/home/About';
 import Hello from '@/components/Hello';
 import Customer from '@/modules/customer/customer';
 import User from '@/modules/user/user';
-import UserList from '@/modules/user/children/list';
-import UserDetail from '@/modules/user/children/detail';
+import UserList from '@/modules/user/views/list';
+import UserDetail from '@/modules/user/views/detail';
 import Activity from '@/modules/activity/activity';
 import Quotation from '@/modules/quotation/quotation';
 
 
 Vue.use(Router);
 
-var helloRoute = {path: '/hello',      name: 'crm.hello',  component: Hello};
-var aboutRoute = {  path: '/about',  name: 'crm.About',  component: About};
-var customerRoute = {  path: '/customer',  name: 'crm.customer',  component: Customer};
-var activityRoute= {path: '/activity',name: 'crm.activity',component: Activity};
-var quotationRoute={path: '/quotation',name: 'crm.quotation',component: Quotation};
-var userRoute = {path: '/user',name: 'crm.user',component: User};
-userRoute.children=[
-  {  path: '',  name: 'crm.user.list',  component: UserList},
-  {  path: 'detail',  name: 'crm.user.detail',  component: UserDetail}
+var helloRoute = { path: '/hello', name: 'crm.hello', component: Hello };
+var aboutRoute = { path: '/about', name: 'crm.About', component: About };
+var customerRoute = { path: '/customer', name: 'crm.customer', component: Customer };
+var activityRoute = { path: '/activity', name: 'crm.activity', component: Activity };
+var quotationRoute = { path: '/quotation', name: 'crm.quotation', component: Quotation };
+var userRoute = { path: '/user', name: 'crm.user', component: User };
+userRoute.children = [
+    { path: '', name: 'crm.user.list', component: UserList },
+    { path: 'detail', name: 'crm.user.detail', component: UserDetail }
 ];
 
-var routeCrm = {  path: '/',  name: 'crm',  component: Desktop};
-routeCrm.children=[   helloRoute,  userRoute,  aboutRoute,  customerRoute,  activityRoute,  quotationRoute  ];
-export default new Router({routes: [routeCrm]});
+var routeCrm = { path: '/', name: 'crm', component: Desktop };
+routeCrm.children = [helloRoute, userRoute, aboutRoute, customerRoute, activityRoute, quotationRoute];
+export default new Router({ routes: [routeCrm] });
